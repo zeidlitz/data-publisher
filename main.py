b3388afccb0a0ac082c24dc57e66bb8d5660bbe8
zeidlitz <zeidlitzerik@gmail.com>
2025-03-10 21:22:03 +0100

    Fixing another typo

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def get_sentiment(label):
 
 def update_metrics(data):
     for entry in data:
-        sentiment = get_sentiment(data["sentiment"])
+        sentiment = get_sentiment(entry["sentiment"])
         labels = ({entry["category"], entry["source"], entry["subsource"]})
         if labels not in gauges:
             gauges[labels] = Gauge(
