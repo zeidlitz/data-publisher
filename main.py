7bbed45ac4e45c31bfbaf96c888cf81230b5bdd3
zeidlitz <zeidlitzerik@gmail.com>
2025-03-10 21:50:45 +0100

    Adding new gauge logic

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import logging
 import json
 from prometheus_client import Gauge, start_http_server
 
-gauges = {}
+gauges = set()
+sentiment_gauge = Gauge(
+        f"sentiment_analysis",
+        "sentiment for given category in source",
+        ["category", "source", "subsource"]
+        )
 
 DATA_SOURCE = "data.json"
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
@@ -15,23 +20,14 @@ def get_sentiment(label):
         return -1
     return 0
 
-def get_valid_prometheous_gauge_name(text):
-    return text.replace("%", "percent").replace("+","plus").replace("/","_").replace("-","_")
-
 def update_metrics(data):
     for entry in data:
         sentiment = get_sentiment(entry["sentiment"])
         for category in entry["category"]:
             labels = (category, entry["source"], entry["subsource"])
             if labels not in gauges:
-                c = get_valid_prometheous_gauge_name(category)
-                # Really need to think about how to do this. I don't want to stop producing metrics if one bad category name gets through. Maybe a try/catch caluse can solve it.
-                gauges[labels] = Gauge(
-                        f"sentiment_{c}",
-                        "sentiment for given category in source",
-                        ["category", "source", "subsource"]
-                        )
-            gauges[labels].labels(*labels).inc(sentiment)
+                gauges.add(labels)
+            sentiment_gauge.labels(labels).inc(sentiment)
 
 def load_data():
     with open(DATA_SOURCE, "r", encoding="utf-8") as f:
