88e1a03644ab2abe5b563cfef2b4a496244e31e8
zeidlitz <zeidlitzerik@gmail.com>
2025-03-11 22:38:54 +0100

    Adding redis consumer logic
    Instead of reading from a file publisher now consumes its input data
    from a redis stream. Also adding python main function conventions.

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+import os
 import time
 import logging
 import json
+import redis
+
+from dotenv import load_dotenv
 from prometheus_client import Gauge, start_http_server
+
+load_dotenv()
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+CONSUMER_STREAM = os.getenv("CONSUMER_STREAM", "data_analysis")
+CONSUMER_GROUP = os.getenv("CONSUMER_GROUP ","data_publisher")
+CONSUMER_NAME = os.getenv("CONSUMER_NAME","publisher")
+REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 
 gauges = set()
 sentiment_gauge = Gauge(
@@ -10,8 +23,8 @@ sentiment_gauge = Gauge(
         ["category", "source", "subsource"]
         )
 
-DATA_SOURCE = "data.json"
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+redis_client = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
 
 def get_sentiment(label):
     if label == "POSITIVE":
@@ -31,15 +44,27 @@ def update_metrics(data):
                 gauges.add(labels)
             sentiment_gauge.labels(category, source, subsource).inc(sentiment)
 
-def load_data():
-    with open(DATA_SOURCE, "r", encoding="utf-8") as f:
-        input_data = json.load(f)
-    return input_data
+def consume_stream():
+    while True:
+        try:
+            messages = redis_client.xreadgroup(CONSUMER_GROUP, CONSUMER_NAME, {CONSUMER_STREAM: '>'}, count=1, block=5000)
+            logging.info(f"Consuming messages from {CONSUMER_STREAM}")
+            if messages:
+                logging.info(f"Message received!")
+                for stream, message_list in messages:
+                    for message_id, message in message_list:
+                        redis_client.xack(CONSUMER_STREAM, CONSUMER_GROUP, message_id)
+                        return json.loads(message['data'])
+        except Exception as e:
+            print(f"Error: {e}")
 
-if __name__ == "__main__":
+def main():
     start_http_server(8000)
-    data = load_data()
     while True:
         logging.info("update_metrics")
+        data = consume_stream()
         update_metrics(data)
         time.sleep(120)
+
+if __name__ == "__main__":
+    main()
