d601f7bd78a0701a693e034aec40167385b56602
zeidlitz <zeidlitzerik@gmail.com>
2026-03-10 11:52:34 +0100

    feat: complete core logic for v2

--- cmd/publisher.go
+++ cmd/publisher.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
 	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/redis/go-redis/v9"
 	"github.com/zeidlitz/data-publisher/internal/config"
 	"github.com/zeidlitz/data-publisher/internal/database"
+	"github.com/zeidlitz/data-publisher/internal/worker"
 )
 
 var version = "dev"
@@ -24,4 +29,17 @@ func main() {
 	}
 
 	redisClient := redis.NewClient(&redis.Options{Addr: cfg.RedisAddr})
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
+	worker := worker.NewWorker(db, redisClient, cfg)
+
+	slog.Info("data-publisher running", "version", version)
+
+	if err := worker.Run(ctx); err != nil {
+		slog.Error("exiting", "err", err)
+	}
+
+	slog.Info("shutting down")
 }
